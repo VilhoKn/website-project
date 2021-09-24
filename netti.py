06de7cfd817963ea43b59ae8792a57440bef30f5
unknown <knuutila.vilho@gmail.com>
2021-09-24 16:17:28 +0300

    added error pages and flex box centering

--- netti.py
+++ netti.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, abort
 import datetime
 
 app = Flask(__name__)
@@ -18,6 +18,13 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
